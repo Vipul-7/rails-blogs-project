be05ca8de0da1d3083b0b4d95f0f35175f3bdc90
vipul chaudhary <vipschaudhary044@gmail.com>
2024-03-04 22:51:24 +0530

    action rich text editor implemented

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -58,7 +58,7 @@ class BlogPostsController < ApplicationController
 
     # a helper function to permit the params to put in database
     def blog_post_params
-        params.require(:blog_post).permit(:title,:body,:published_at)
+        params.require(:blog_post).permit(:title,:content,:published_at)
     end
 
     def set_blog_post
