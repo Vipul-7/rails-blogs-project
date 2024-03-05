bb9359da559591977222cbda00df924c1086d04a
vipul chaudhary <vipschaudhary044@gmail.com>
2024-03-05 18:16:42 +0530

    feat: cover image added

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -58,7 +58,7 @@ class BlogPostsController < ApplicationController
 
     # a helper function to permit the params to put in database
     def blog_post_params
-        params.require(:blog_post).permit(:title,:content,:published_at)
+        params.require(:blog_post).permit(:title,:content,:published_at,:cover_image)
     end
 
     def set_blog_post
